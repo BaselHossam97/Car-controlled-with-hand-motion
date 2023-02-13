c09f57120ab2fdcfbf773969793ad33ffd7a0614
BaselHossam97 <14712020102067@eng-st.cu.edu.eg>
2023-02-13 05:24:58 +0200

    Uploading project files

---
+++ Debug/MCAL/ADC/ADC_Prog.d
@@ -0,0 +1,14 @@
+MCAL/ADC/ADC_Prog.o MCAL/ADC/ADC_Prog.o: ../MCAL/ADC/ADC_Prog.c \
+  ../MCAL/ADC/ADC_Interface.h ../MCAL/ADC/ADC_Config.h \
+  C:\Users\husse\workspace\slavenew/BitMath.h \
+  C:\Users\husse\workspace\slavenew/STD.h ../MCAL/ADC/ADC_Private.h
+
+../MCAL/ADC/ADC_Interface.h:
+
+../MCAL/ADC/ADC_Config.h:
+
+C:\Users\husse\workspace\slavenew/BitMath.h:
+
+C:\Users\husse\workspace\slavenew/STD.h:
+
+../MCAL/ADC/ADC_Private.h:
